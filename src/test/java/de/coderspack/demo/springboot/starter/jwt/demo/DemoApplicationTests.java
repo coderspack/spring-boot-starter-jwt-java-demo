420faa8bdf43e599fd2956904e9e875e95cfd586
larmic <mail@larmic.de>
2019-11-21 18:51:40 +0100

    Add simple spring boot app

---
+++ src/test/java/de/coderspack/demo/springboot/starter/jwt/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.coderspack.demo.springboot.starter.jwt.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
